cd1b77d8cced66b4813c35bebd4cd1c7e06c37d2
Neko-Sato <hisatosi35350@gmail.com>
2021-08-04 04:19:32 +0900

    step1

--- lib/components/app/top_screens/home.dart
+++ lib/components/app/top_screens/home.dart
@@ -13,17 +13,37 @@ class _HomeState extends State<Home> {
   String name = 'name';
   String userId = 'user_id';
   Widget image = Icon(Icons.account_circle, size: 100.0);
+  int followsNum = 0;
+  int followersNum = 0;
   _HomeState() {
     Configs.fontalk.account.info().then((value) {
       this.name = value['name'];
       this.userId = value['user_id'];
       if (value['image'] != null)
         this.image = Image.memory(value['image'], width: 100.0, height: 100.0);
+      this.followsNum = value['follows_num'];
+      this.followersNum = value['followers_num'];
       setState(() {});
     });
   }
   @override
   Widget build(BuildContext context) {
-    return Column(children: [Text(this.name), Text(this.userId), this.image]);
+    return Column(children: [
+      Row(children: [
+        this.image,
+        Column(
+          children: [
+            Text(this.name),
+            Text("@" + this.userId),
+          ],
+        )
+      ]),
+      Row(
+        children: [
+          Text("follows: " + this.followsNum.toString()),
+          Text("followers: " + this.followersNum.toString()),
+        ],
+      )
+    ]);
   }
 }
